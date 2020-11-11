c0ebe7e5f02dd165827119c0e1c307b6e95205c4
WhereIsMyHead <gene_zhangchen@163.com>
2020-11-11 19:34:32 +0800

    Update dependency_dist.py

--- dependency_dist.py
+++ dependency_dist.py
@@ -3,7 +3,21 @@
 import numpy as np
 import networkx as nx
 import spacy
+
+from spacy.tokens import Doc
+
+class WhitespaceTokenizer(object):
+    def __init__(self, vocab):
+        self.vocab = vocab
+
+    def __call__(self, text):
+        words = text.split()
+        # All tokens 'own' a subsequent space character in this tokenizer
+        spaces = [True] * len(words)
+        return Doc(self.vocab, words=words, spaces=spaces)
+
 nlp = spacy.load('en_core_web_sm')
+nlp.tokenizer = WhitespaceTokenizer(nlp.vocab)
 
 def dependency_dist_func(text, aspect_term):
     # https://spacy.io/docs/usage/processing-text
